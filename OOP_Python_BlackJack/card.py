519d9e932dd531cd720a50afbbb405fefdf782a3
stebbinscp <cstebbins@uchicago.edu>
2021-02-24 19:55:51 -0600

    Create card.py

---
+++ OOP_Python_BlackJack/card.py
@@ -0,0 +1,81 @@
+# Remember to use docstrings in your classes and functions!
+
+import tkinter as tk
+from PIL import Image, ImageTk
+import os
+
+class Card:
+    """
+    Object class which stors the suit and value of the given
+    card, loads the images as a class method, and has two properties:
+    size, and image. 
+
+    TO LOAD IMAGES Ensure that the current working directory is within the blackjack folder.
+    """
+ 
+    CLUBS = "C"
+    DIAMONDS = "D"
+    HEARTS = "H"
+    SPADES =  "S" 
+
+    def __init__(self, suit, card):
+        """Initiate the card class with a suit and value."""
+        # self.suit = self.set_suit(suit)
+        # self.value = self.set_value(value)
+        self.suit = suit
+        self.card = card
+
+    @classmethod
+    def load_images(self):
+        """
+        Load in images for every card into a class attribute self.card_images
+        in tuples of (card name, tkinter photo object)
+        """
+        suits = ['H', 'C', 'D', 'S']
+        face_cards = ['J', 'K', 'Q', "A", "10"]
+        self.card_images = [] 
+
+        path = "/images"
+        pre_file_pre = os.getcwd()
+        pre_file = pre_file_pre+path
+        
+        for suit in suits:
+            for value in range(2, 10):
+                card = '{}{}'.format(str(value), suit)
+                card_name = "/"+card+".jpg"
+                img = Image.open(pre_file+card_name)
+                img = img.resize((100,150), Image.ANTIALIAS)
+                image = ImageTk.PhotoImage(img)
+                self.card_images.append((card, image))
+            for card in face_cards:
+                card = '{}{}'.format(str(card), suit)
+                card_name = "/"+card+".jpg"
+                img = Image.open(pre_file+card_name)
+                img = img.resize((100,150), Image.ANTIALIAS)
+                image = ImageTk.PhotoImage(img)
+                self.card_images.append((card, image))
+
+    @property
+    def value(self):
+        if self.card in ["2", "3", "4", "5", "6", "7", "8", "9", "10", "A", "K", "Q", "J"]:
+            # print(self.card)
+            return self.card
+        else:
+            # print(self.card) 
+            raise ValueError("The inputted card value is not within specifications.")
+        
+    @property
+    def image(self):
+        if self.card == "10":
+            key = str(self.card[0:2])+str(self.suit[0:1].upper())
+        else:
+            key = str(self.card[0:1])+str(self.suit[0:1].upper())
+        for i in range(52):
+            if self.card_images[i][0] == key:
+                return self.card_images[i][1] # should return the object
+
+
+# path = "/images"
+# pre_file_pre = os.getcwd()
+# pre_file = pre_file_pre+path
+# print(pre_file)
